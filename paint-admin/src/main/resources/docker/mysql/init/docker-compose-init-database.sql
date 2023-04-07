1de850fecc874ab6eb3238537612200bb6928b0e
woxigousade <woxigsd@gmail.com>
2023-04-07 18:23:08 +0800

    feat: docker-compose部署所需文件添加

--- paint-admin/src/main/resources/docker/mysql/init/docker-compose-init-database.sql
+++ paint-admin/src/main/resources/docker/mysql/init/docker-compose-init-database.sql
@@ -1,3 +1,3 @@
-# 放入docker-compose.yaml ./init目录下
+# 放入docker-compose.yaml ./mysql/init目录下，用于mysql数据库初始化后执行，创建业务服务所需数据库
 drop database if exists `ai_paint`;
 CREATE DATABASE `ai_paint` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
